88ca0e9237729a17e1a69b7bf5eb66df1d070a94
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-05-31 15:32:32 +0530

    Update app.js
    removeconflic

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.use(cookieParser());
 //import All routers
 const user = require('./routes/user');
 const session = require('./routes/session')
-// const research = require('./routes/user');
-// const sessions = require('./routes/user');
-// const payments = require('./routes/user');
-
 
 app.use('/api/v1/', user)
 app.use('/api/v1/', session)
